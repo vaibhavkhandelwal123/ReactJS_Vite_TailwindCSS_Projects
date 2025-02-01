4ea4b6a55506d9186c0629642634023fde0c2ec3
khandelwalvaibhav123 <khandelwalvaibhav123456@gmail.com>
2025-02-02 03:10:45 +0530

    first commit

---
+++ Tailwind_ReactJS_Vite_01_BG_Changer/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen'>
+    <h1 className='text-white justify-center font-bold '>hello world</h1>
+    </div>
+  )
+}
+
+export default App
