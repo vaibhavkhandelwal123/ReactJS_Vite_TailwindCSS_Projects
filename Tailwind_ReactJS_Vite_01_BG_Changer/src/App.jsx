71ebfc865cb54c0a3b2397aa0703552b4e7c1280
khandelwalvaibhav123 <khandelwalvaibhav123456@gmail.com>
2025-02-05 11:26:09 +0530

    bg changer

--- Tailwind_ReactJS_Vite_01_BG_Changer/src/App.jsx
+++ Tailwind_ReactJS_Vite_01_BG_Changer/src/App.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-
+import { useState } from "react"
 const App = () => {
-  return (
-    <div className='bg-black h-screen'>
-    <h1 className='text-white justify-center font-bold '>hello world</h1>
-    </div>
-  )
-}
+  const [color, setColor] = useState("yellow");
 
-export default App
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg px-3 py-2 bg-white rounded-3xl ">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("pink")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+          <button
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "olive" }}
+          >
+            Olive
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
