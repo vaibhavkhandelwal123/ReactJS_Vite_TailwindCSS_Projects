cd9fdaf1d88c6435661d75395c7f67cce35e043f
khandelwalvaibhav123 <khandelwalvaibhav123456@gmail.com>
2025-02-25 19:35:34 +0530

    react Router

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+   <div>
+    <h1 className='flex bg-gray-700 justify-center text-white text-3xl py-2 m-2'>React-Router</h1>
+   </div>
+  )
+}
+
+export default App
